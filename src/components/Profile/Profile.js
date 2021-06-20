3daa30cdc11177c1fef3914273d287040a9bae36
Sick-Carlito <carlb1319@gmail.com>
2021-06-20 13:53:10 +0300

    Build a basic timeline component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,26 +1,28 @@
-import { Typography } from '@material-ui/core';
-import React from 'react';
-import './Profile.css';
+import { Typography } from "@material-ui/core";
+import React from "react";
+import "./Profile.css";
+import CustomTimeline from "../Timeline/Timeline.js";
+import profileImage from "../../assets/images/profileImage.jpeg";
 
 const Profile = () => {
-    return (
-        <div className ='profile container_shadow'> 
-            <div className = 'profile_name'>
-                <Typography className = "name">Carl</Typography>
-                <Typography className = "title">Carlito</Typography>
-            </div>
+  return (
+    <div className="profile container_shadow">
+      <div className="profile_name">
+        <Typography className="name">Carl</Typography>
+        <Typography className="title">Carlito</Typography>
+      </div>
 
-            <figure className = "profile_image">
-                <img src={require('../../assets/images/profileImage.jpeg')} alt="carl" />
-            </figure>
+      <figure className="profile_image">
+        <img src={profileImage} alt="carl" />
+      </figure>
 
-            <div className="profile_information">
-                Insert Timeline
-                <br/>
-                <button>My button</button>
-            </div>
-        </div>
-    )
-}
+      <div className="profile_information">
+        <CustomTimeline />
+        <br />
+        <button>My button</button>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
