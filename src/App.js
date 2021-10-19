a5324dc830a7fdb1462c8f426be6f1c5ec95adcd
Sick-Carlito <carlb1319@gmail.com>
2021-10-19 10:46:26 +0300

    Modified app.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import {Container, Grid} from '@material-ui/core';
-import './App.css';
+import React from "react";
+import { Container, Grid } from "@material-ui/core";
+import "./App.css";
 
-import Profile from './components/Profile/Profile';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Portfolio from './pages/Portfolio/Portfolio';
-import Resume from './pages/Resume/Resume';
+import Profile from "./components/Profile/Profile";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Resume from "./pages/Resume/Resume";
 
-
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Container>
-      <Grid container>
-        <Grid  item xs={12} sm={12} md={4} lg={3}>
-          <Profile/>
+      <Grid container spacing={7}>
+        <Grid item xs={12} sm={12} md={4} lg={3}>
+          <Profile />
         </Grid>
-        <Grid  item xs>
-          <Header/>
+        <Grid item xs>
+          <Header />
           <Router>
             <Switch>
-            <Route path='/portfolio'>
-              <Portfolio/>
-            </Route>
-            <Route path='/'>
-             <Resume/>
-            </Route>
+              <Route path="/portfolio">
+                <Portfolio />
+              </Route>
+              <Route path="/">
+                <Resume />
+              </Route>
             </Switch>
-            
           </Router>
-          <Footer/>
+          <Footer />
         </Grid>
       </Grid>
     </Container>
